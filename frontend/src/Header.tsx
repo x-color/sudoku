a6598406cc34541786f871afa3aed86385cb2457
x-color <36035885+x-color@users.noreply.github.com>
2021-06-23 00:36:10 +0900

    Add header component

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+
+export const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          数独解答プログラム
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
