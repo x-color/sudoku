c2bcf8bac82a251f3f3c49a901a0eba9a0182733
x-color <36035885+x-color@users.noreply.github.com>
2021-07-04 14:20:40 +0900

    Modify function definition of App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,13 @@ import "./App.css";
 import { SudokuBox } from "./components/Sudoku";
 import { Header } from "./components/Header";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <Header></Header>
       <SudokuBox></SudokuBox>
     </div>
   );
-}
+};
 
 export default App;
